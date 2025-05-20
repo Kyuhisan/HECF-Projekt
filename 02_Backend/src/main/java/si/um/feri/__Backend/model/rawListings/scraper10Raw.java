9f4c1062f14310500f19f81da18ed9a9d45b4a47
Kyuhi <matickuhar1999@gmail.com>
2025-05-20 15:08:21 +0200

    updated chromedriver paths

--- 02_Backend/src/main/java/si/um/feri/__Backend/model/rawListings/scraper10Raw.java
+++ 02_Backend/src/main/java/si/um/feri/__Backend/model/rawListings/scraper10Raw.java
@@ -9,25 +9,11 @@ public class scraper10Raw {
     private String source;
     private String reference;
     private String summary;
-    private Scraper10Metadata metadata;
-
-
-    @Data
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    public static class Scraper10Metadata {
-        private String summary;
-        private String id;
-        private String url;
-        private String deadlineDate;
-        private String technology;
-        private String domains;
-        private String typeOfBeneficiary;
-        private String maxFunding;
-
-
-    }
+    private String id;
+    private String url;
+    private String deadlineDate;
+    private String technology;
+    private String domains;
+    private String typeOfBeneficiary;
+    private String maxFunding;
 }
-
-
-
-
--- 02_Backend/src/main/java/si/um/feri/__Backend/model/rawListings/scraper20Raw.java
+++ 02_Backend/src/main/java/si/um/feri/__Backend/model/rawListings/scraper20Raw.java
@@ -6,23 +6,14 @@ import lombok.Data;
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class scraper20Raw {
-
     private String source;
     private String reference;
     private String summary;
-    private Scraper20Metadata metadata;
-
-    @Data
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    public static class Scraper20Metadata {
-        private String summary;
-        private String id;
-        private String url;
-        private String deadlineDate;
-        private String technology;
-        private String status;
-        // private String massiveText
-        //private String guidelines
-
-    }
+    private String id;
+    private String url;
+    private String deadlineDate;
+    private String technology;
+    private String status;
+    //  private String massiveText
+    //  private String guidelines
 }

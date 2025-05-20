502c6419567c03fcc39b92fdf72bdda0f3a32b18
TilenBrunec <tilen.brunec@student.um.si>
2025-05-20 14:36:08 +0200

    added raw structure

--- 02_Backend/src/main/java/si/um/feri/__Backend/model/rawListings/scraper10Raw.java
+++ 02_Backend/src/main/java/si/um/feri/__Backend/model/rawListings/scraper10Raw.java
@@ -1,7 +1,33 @@
 package si.um.feri.__Backend.model.rawListings;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class scraper10Raw {
+    private String source;
+    private String reference;
+    private String summary;
+    private Scraper10Metadata metadata;
+
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Scraper10Metadata {
+        private String summary;
+        private String id;
+        private String url;
+        private String deadlineDate;
+        private String technology;
+        private String domains;
+        private String typeOfBeneficiary;
+        private String maxFunding;
+
+
+    }
 }
+
+
+
+
--- 02_Backend/src/main/java/si/um/feri/__Backend/model/rawListings/scraper20Raw.java
+++ 02_Backend/src/main/java/si/um/feri/__Backend/model/rawListings/scraper20Raw.java
@@ -1,7 +1,28 @@
 package si.um.feri.__Backend.model.rawListings;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class scraper20Raw {
+
+    private String source;
+    private String reference;
+    private String summary;
+    private Scraper20Metadata metadata;
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Scraper20Metadata {
+        private String summary;
+        private String id;
+        private String url;
+        private String deadlineDate;
+        private String technology;
+        private String status;
+        // private String massiveText
+        //private String guidelines
+
+    }
 }

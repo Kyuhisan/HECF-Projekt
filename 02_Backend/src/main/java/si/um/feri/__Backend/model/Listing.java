4487d98ed1c5a752471513a5f97bb5bad5543c46
Kyuhi <matickuhar1999@gmail.com>
2025-05-23 01:31:01 +0200

    updated scraper templates

--- 02_Backend/src/main/java/si/um/feri/__Backend/model/Listing.java
+++ 02_Backend/src/main/java/si/um/feri/__Backend/model/Listing.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.List;
 
-@Document(collection = "listings")
+@Document(collection = "listingsTesting")
 @Data
 public class Listing {
     @Id
